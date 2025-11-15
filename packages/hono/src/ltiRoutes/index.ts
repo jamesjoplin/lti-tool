242f1592a31bfb491d951296a512781a6e21fd65
jamesjoplin <james.e.joplin@gmail.com>
2025-11-15 23:24:29 +0000

    feat: implement dynamic registration hono route handlers #10

--- packages/hono/src/ltiRoutes/index.ts
+++ packages/hono/src/ltiRoutes/index.ts
@@ -1,5 +1,9 @@
 // route exports
 export { deepLinkRouteHandler } from './routes/deepLink.route';
+export {
+  completeDynamicRegistrationRouteHandler,
+  initiateDynamicRegistrationRouteHandler,
+} from './routes/dynamicRegistration.route';
 export { jwksRouteHandler } from './routes/jwks.route';
 export { launchRouteHandler } from './routes/launch.route';
 export { loginRouteHandler } from './routes/login.route';
