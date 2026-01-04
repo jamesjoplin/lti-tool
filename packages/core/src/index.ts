f7125e8cf42e8d3b3ad8c0ad0d003b5e8620d1e3
jamesjoplin <james.e.joplin@gmail.com>
2026-01-04 18:31:39 +0000

    feat: export serverless env check function

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from './interfaces/index.js';
 export * from './schemas/index.js';
+export { isServerlessEnvironment } from './utils/environment.js';
 export { LTITool } from './ltiTool.js';
